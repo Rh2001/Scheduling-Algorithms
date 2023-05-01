ab1cbf570b1cb3f04cd59589f095984ea2582b42
Roham H <100292931+Rh2001@users.noreply.github.com>
2023-05-02 00:17:23 +0330

    Update RoundRobin.cs

--- RoundRobin.cs
+++ RoundRobin.cs
@@ -1,71 +1,73 @@
-﻿
-public class Process
+﻿namespace RR
 {
-    public string Name { get; set; }
-    public int BurstTime { get; set; }
-    public int RemainingTime { get; set; }
-
-    public Process(string name, int burstTime)
+    public class Process
     {
-        Name = name;
-        BurstTime = burstTime;
-        RemainingTime = burstTime;
-    }
-}
+        public string Name { get; set; }
+        public int BurstTime { get; set; }
+        public int RemainingTime { get; set; }
 
-public class RoundRobin
-{
-    public static void Main()
-    {
-        List<Process> processes = new List<Process>();
-
-        // Add processes to the list
-        processes.Add(new Process("P1", 8));
-        processes.Add(new Process("P2", 4));
-        processes.Add(new Process("P3", 9));
-        processes.Add(new Process("P4", 2));
-        processes.Add(new Process("P5", 10));
-
-        int quantum = 4; // Set the time quantum to 2
-
-        RunRoundRobin(processes, quantum);
+        public Process(string name, int burstTime)
+        {
+            Name = name;
+            BurstTime = burstTime;
+            RemainingTime = burstTime;
+        }
     }
 
-    public static void RunRoundRobin(List<Process> processes, int quantum)
+    public class RoundRobin
     {
-        Queue<Process> readyQueue = new Queue<Process>();
-        int totalTime = 0;
-        int numProcesses = processes.Count;
-
-        // Add all processes to the ready queue
-        foreach (Process p in processes)
+        public static void Main()
         {
-            readyQueue.Enqueue(p);
+            List<Process> processes = new List<Process>();
+
+            // Add processes to the list
+            processes.Add(new Process("P1", 8));
+            processes.Add(new Process("P2", 4));
+            processes.Add(new Process("P3", 9));
+            processes.Add(new Process("P4", 2));
+            processes.Add(new Process("P5", 10));
+
+            int quantum = 4; // Set the time quantum to 2
+
+            RunRoundRobin(processes, quantum);
         }
 
-        while (readyQueue.Count > 0)
+        public static void RunRoundRobin(List<Process> processes, int quantum)
         {
-            Process currentProcess = readyQueue.Dequeue();   //Returns the first item in our roundrobin queue
-            Console.WriteLine("Running {0}...", currentProcess.Name);   //Signify which thread is running
+            Queue<Process> readyQueue = new Queue<Process>();
+            int totalTime = 0;
+            int numProcesses = processes.Count;
 
-            if (currentProcess.RemainingTime <= quantum)        //The remainig burst time is less than our timequantom so the process will finish in time
+            // Add all processes to the ready queue
+            foreach (Process p in processes)
             {
-                
-                totalTime += currentProcess.RemainingTime;
-                Console.WriteLine("{0} completed in {1}ms.", currentProcess.Name, currentProcess.RemainingTime);   //We can add thread.sleep to turn it into seconds
-                currentProcess.RemainingTime = 0;
+                readyQueue.Enqueue(p);
             }
-            else
+
+            while (readyQueue.Count > 0)
             {
-                // Process will not finish within our time quantom
-                totalTime += quantum;
-                currentProcess.RemainingTime -= quantum;
-                Console.WriteLine("{0} did not complete. {1}ms remaining.", currentProcess.Name, currentProcess.RemainingTime);
-                readyQueue.Enqueue(currentProcess); // Add process back to the queue
+                Process currentProcess = readyQueue.Dequeue();   //Returns the first item in our roundrobin queue
+                Console.WriteLine("Running {0}...", currentProcess.Name);   //Signify which thread is running
+
+                if (currentProcess.RemainingTime <= quantum)        //The remainig burst time is less than our timequantom so the process will finish in time
+                {
+
+                    totalTime += currentProcess.RemainingTime;
+                    Console.WriteLine("{0} completed in {1}ms.", currentProcess.Name, currentProcess.RemainingTime);   //We can add thread.sleep to turn it into seconds
+                    currentProcess.RemainingTime = 0;
+                }
+                else
+                {
+                    // Process will not finish within our time quantom
+                    totalTime += quantum;
+                    currentProcess.RemainingTime -= quantum;
+                    Console.WriteLine("{0} did not complete. {1}ms remaining.", currentProcess.Name, currentProcess.RemainingTime);
+                    readyQueue.Enqueue(currentProcess); // Add process back to the queue
+                }
             }
+
+            Console.WriteLine("All processes completed in {0}ms.", totalTime);
+            Console.WriteLine("Average turnaround time: {0}ms", (float)totalTime / numProcesses);
         }
-
-        Console.WriteLine("All processes completed in {0}ms.", totalTime);
-        Console.WriteLine("Average turnaround time: {0}ms", (float)totalTime / numProcesses);
     }
 }
